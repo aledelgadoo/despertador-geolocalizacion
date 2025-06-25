c0efbf83b6b325de43fdd129c5f5a460763948e5
aledelgadoo <alejandrodelgadovalera@gmail.com>
2025-06-25 16:25:11 +0100

    Reorganizado en funciones, creado el main que llame a estas funciones. Diseño modular.

--- app.py
+++ app.py
@@ -3,79 +3,126 @@ import folium
 from streamlit_folium import st_folium
 from geopy.distance import geodesic
 
-# Titulos y texto que se mostrará en la página web
-st.title("⏰ Despertador por geolocalización")
-st.write("By Alejandro D.")
 
-radio_alarma = 500 # metros
+def main():
+    '''
+    Genera los títulos de la página web, inicializa las variables necesarias para que funcione
+    la página (radio_alarma, zona_objetivo, ruta), y llama a todas el resto de las funciones
+    '''
 
-# Simulamos una ruta (una lista de coordenadas)
-ruta = [
-    (28.1000, -15.4600),
-    (28.1050, -15.4500),
-    (28.1100, -15.4450),
-    (28.1150, -15.4400),
-    (28.1200, -15.4370),
-    (28.1235, -15.4366),
-]
-st.session_state.setdefault("paso", 0) # Variable paso por defecto 0
+    # Titulos y texto que se mostrará en la página web
+    st.title("⏰ Despertador por geolocalización")
+    st.write("By Alejandro D.")
 
-# Posición actual simulada
-paso_actual = st.session_state.paso
+    radio_alarma = 500 # metros
+    zona_objetivo = (28.1235, -15.4366)
 
-# Activar/Desactivar alarma
-st.session_state.setdefault("alarma_activada", False) # Variable alarma_activada por defecto False
+    # Simulamos una ruta (una lista de coordenadas)
+    ruta = [
+        (28.1000, -15.4600),
+        (28.1050, -15.4500),
+        (28.1100, -15.4450),
+        (28.1150, -15.4400),
+        (28.1200, -15.4370),
+        (28.1235, -15.4366),
+    ]
 
-if st.button("🔘 Activar alarma"):
-    st.session_state.alarma_activada = True
+    controles_alarma(ruta)
+    pos_actual = ruta[st.session_state.paso]
 
-if st.button("🛑 Desactivar alarma"):
-    st.session_state.alarma_activada = False
+    mapa = crear_mapa(pos_actual, zona_objetivo, radio_alarma, ruta)
+    st_data = st_folium(mapa, width=700, height=500)  # Mostramos el mapa en Streamlit
 
-st.info(f"Estado de la alarma: {st.session_state.alarma_activada}") # Mostramos estado alarma
+    distancia = calcular_distancia(pos_actual, zona_objetivo)
+    logica_alarma(distancia, radio_alarma)
 
-# Botón para avanzar por la ruta
-if st.button("🚶 Avanzar al siguiente punto"):
-    if st.session_state.paso < len(ruta) - 1:
-        st.session_state.paso += 1
-pos_actual = ruta[st.session_state.paso]
 
-# Creamos el mapa centrado en esa ubicación
-mapa = folium.Map(location=pos_actual, zoom_start=13)
+def crear_mapa(pos_actual, zona_objetivo, radio_alarma, ruta=None):
+    '''
+    Crea el mapa con el marcador, la zona objetivo y la ruta.
+    Devuelve este propio mapa, sin mostrarlo en la web todavía.
+    '''
 
-# Unimos las coordenadas para mejor visibilidad
-folium.PolyLine(ruta, color="green", weight=2.5, opacity=1).add_to(mapa)
+    # Creamos el mapa
+    mapa = folium.Map(location=pos_actual, zoom_start=13)
 
-# Ponemos un marcador en esa ubicación
-folium.Marker(
-    location=pos_actual, # Marcador en la ub. marcada en los sliders
-    tooltip="Aquí estoy",
-    icon=folium.Icon(color="red", icon="home")
-).add_to(mapa)
+    # Ponemos un marcador en la ubicación del usuario
+    folium.Marker(
+        location=pos_actual, # Marcador en la ub. marcada en los sliders
+        tooltip="Aquí estoy",
+        icon=folium.Icon(color="red", icon="home")
+    ).add_to(mapa)
 
-# Creamos un circulo alrededor de la zona objetivo
-zona_objetivo = (28.1235, -15.4366)
-folium.Circle(
-    location=zona_objetivo,
-    radius=radio_alarma, # Para que coincida con la alarma
-    color='blue',
-    fill=True,
-    fill_opacity=0.2,
-    tooltip="Zona objetivo"
-).add_to(mapa)
+    # Creamos un circulo alrededor de la zona objetivo
+    folium.Circle(
+        location=zona_objetivo,
+        radius=radio_alarma, # Para que coincida con la alarma
+        color='blue',
+        fill=True,
+        fill_opacity=0.2,
+        tooltip="Zona objetivo"
+    ).add_to(mapa)
 
-# Mostramos el mapa en Streamlit
-st_data = st_folium(mapa, width=700, height=500)
+    # Unimos las coordenadas para mejor visibilidad
+    if ruta:
+        folium.PolyLine(ruta, color="green", weight=2.5, opacity=1).add_to(mapa)
 
-# Calculamos la distancia 
-distancia = geodesic(pos_actual, zona_objetivo).meters
-st.write(f"Distancia actual: {distancia:.2f}metros")
+    return mapa
 
-# Lógica de la alarma
-if st.session_state.alarma_activada:
-    if distancia <= radio_alarma: # Dentro de la zona objetivo
-        st.success("¡¡¡¡¡¡DESPIERTA!!!!!!!")
+
+def controles_alarma(ruta):
+    '''
+    Permite activar/desactivar la alarma, muestra el estado actual. 
+    Contiene también el botón para avanzar por la ruta.
+    '''
+    # Valores default de los session_state
+    st.session_state.setdefault("paso", 0)
+    st.session_state.setdefault("alarma_activada", False)
+
+    if st.button("🔘 Activar alarma"):
+        st.session_state.alarma_activada = True
+
+    if st.button("🛑 Desactivar alarma"):
+        st.session_state.alarma_activada = False
+
+    # Mostramos estado actual de la alarma
+    st.info(f"Estado de la alarma: {st.session_state.alarma_activada}")
+
+    # Botón para avanzar por la ruta
+    avanzar = False
+    if st.button("🚶 Avanzar al siguiente punto"):
+        if st.session_state.paso < len(ruta) - 1:
+            st.session_state.paso += 1
+            avanzar = True
+    
+    return avanzar
+
+
+def logica_alarma(distancia, radio_alarma):
+    '''
+    Muestra el estado actual de la alarma y la distancia desde la ubicación actual hasta la zona objetivo.
+    Contiene la lógica de la alarma
+    '''
+   
+    # Mostramos la distancia actual
+    st.write(f"Distancia actual: {distancia:.2f} metros")
+
+    # Lógica de la alarma
+    if st.session_state.alarma_activada:
+        if distancia <= radio_alarma: # Dentro de la zona objetivo
+            st.success("¡¡¡¡¡¡DESPIERTA!!!!!!!")
+        else:
+            st.success("Todavía no ha sonado la alarma, descansa...")
     else:
-        st.success("Todavía no ha sonado la alarma, descansa...")
-else:
-    st.warning("La alarma no está encendida")
+        st.warning("La alarma no está encendida")
+
+
+def calcular_distancia(p1, p2):
+    '''
+    Calcula la distancia entre 2 puntos, en metros.
+    '''
+    return geodesic(p1,p2).meters
+
+
+if __name__ == "__main__":
+    main()
