c27f2adf0e2426ea30bc8213d47f11c7288d4c3b
aledelgadoo <alejandrodelgadovalera@gmail.com>
2025-06-26 16:21:36 +0100

    Añadido texto explicativo de la app en la página principal.

--- app.py
+++ app.py
@@ -14,7 +14,16 @@ def main():
     st.title("⏰ Despertador por geolocalización")
     st.write("By Alejandro D.")
     st.info("Usa el menú de la izquierda para navegar entre páginas 🤝")
+    st.write("""
+             ¿Cansado de despertarte siempre a la misma hora sin importar dónde estés? ¡Esta app cambia las reglas del juego!
 
+            Con el Despertador por Geolocalización que he diseñado, podrás configurar una zona objetivo geográfica y un radio de alerta. Cuando te acerques a ese lugar, la alarma se activará automáticamente, ayudándote a despertarte justo en el momento y lugar indicados, sin depender del horario fijo.
+
+            Ideal para viajeros, estudiantes o cualquier persona que necesite despertarse según su ubicación actual, no la hora del reloj. Además, la interfaz intuitiva y el mapa interactivo te permiten seleccionar fácilmente tu punto de partida y destino, simulando el recorrido para una experiencia personalizada y precisa.
+
+            ¡Despierta a tiempo, donde sea que estés!
+
+            """)
 
 if __name__ == "__main__":
     main()
